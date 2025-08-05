2529ae3cf5dec6235951b182814bb2ee033c2545
ValeriiGithub <valeri.github@gmail.com>
2025-08-05 11:25:42 +0300

    Интеграция Pytest и Playwright

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_user_login():
     print('Hello!')
 
@@ -15,3 +18,11 @@ def test_assert_positive_case():
 
 def test_assert_negative_case():
     assert (2 + 2) == 5, '(2 + 2) != 5'
+
+
+def test_zero_division(): 
+    with pytest.raises(ZeroDivisionError): 
+        1 / 0
+
+def test_lists():
+    assert [1, 2, 3] == [1, 2, 4]
