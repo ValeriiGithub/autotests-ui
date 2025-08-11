d0b1747b37526da12530b9a0db8c7c04b38fe675
ValeriiGithub <valeri.github@gmail.com>
2025-08-11 16:48:42 +0300

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,7 @@
 import pytest
 
-from pages.courses_list_page import CheckVisibleCourseCardParams, CoursesListPage
+from components.courses.course_view_component import CheckVisibleCourseCardParams
+from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
 
@@ -11,8 +12,7 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
         title="", 
         max_score="0", 
@@ -25,8 +25,8 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image(file=r'.\testdata\files\image.png')
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title = "Playwright",
         estimated_time = "2 weeks",
@@ -35,10 +35,11 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
         min_score = "10"
     )
     create_course_page.click_create_course_button()
+    
+    courses_list_page.toolbar_view.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
+    # courses_list_page.check_visible_course_card(
+    courses_list_page.course_view.check_visible(
         CheckVisibleCourseCardParams(
             index= 0,
             title = "Playwright",
@@ -58,7 +59,6 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible()
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
